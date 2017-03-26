3201f36600e4d9da7c3540effdd88999b058a80f
DESKTOP-11156IH\ASUS <kanjanaroot_K@cmu.ac.th>
2017-03-26 12:24:27 +0700

    2.1 new 4 Pp

--- src/main/java/camt/se234/unittest/dao/UserDaoImpl.java
+++ src/main/java/camt/se234/unittest/dao/UserDaoImpl.java
@@ -27,6 +27,15 @@ public class UserDaoImpl {
         users.add(new User("Thapakorn","5615","OAT",
                 LocalDate.of(2000,8,30),"08187654321"));
 
+        users.add(new User("KKK","1235","AFK",
+                LocalDate.of(1989,12,14),"08512341678"));
+        users.add(new User("Kimpan","5676","J",
+                LocalDate.of(1999,4,30),"09687654311"));
+        users.add(new User("Katip","1034","Xax",
+                LocalDate.of(1977,12,4),"08622345671"));
+        users.add(new User("Thapanis","5415","GOAT",
+                LocalDate.of(2017,5,10),"09187654321"));
+
     }
 
     public List<User> getUsers(){
