f81bbb8d0bc46addd6fd177e5accb7b3e9d32bc0
P0G8HNSKR2C0O8B\Administrator <kitsada_tang@cmu.ac.th>
2017-03-26 11:20:21 +0700

    Add isAbletogotoPub test

--- src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
+++ src/test/java/camt/se234/unittest/dao/UserDaoImplTest.java
@@ -8,6 +8,7 @@ import org.junit.rules.ExpectedException;
 
 import java.time.LocalDate;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 
@@ -27,23 +28,17 @@ public class UserDaoImplTest {
     @Rule
     public ExpectedException thrown = ExpectedException.none();
 
-    @Test
-    public void testLoginException(){
-        UserServiceImpl userService = new UserServiceImpl();
-        UserDaoImpl userDao = new UserDaoImpl();
-        userService.setUserDao(userDao);
-        // check for the exception we expect
-        thrown.expect(NullPointerException.class);
-        userService.login("","");
-    }
-    @Test
-    public void testgetPubAllowanceUser(){
-        UserServiceImpl userService = new UserServiceImpl();
-        UserDaoImpl userDao = new UserDaoImpl();
-        userService.setUserDao(userDao);
-        assertThat(userService.getPubAllowanceUser( LocalDate.of(2017,3,20)), hasItems(new User("Prayuth","1234","Tu",
-                LocalDate.of(1979,2,14),"08612345678")));
-    }
 
+    @Test
+    public void testisAbleToGoToPub(){
+        UserServiceImpl userService = new UserServiceImpl();
+        UserDaoImpl userDao = new UserDaoImpl();
+        userService.setUserDao(userDao);
+        assertThat(userService.isAbleToGoToPub(userService.login("Prayuth","1234"), LocalDate.of(2017,3,20)), is(true));
+        assertThat(userService.isAbleToGoToPub(userService.login("Tucky","5675"), LocalDate.of(2017,3,20)), is(false));
+        assertThat(userService.isAbleToGoToPub(userService.login("Honey","aabbcc"), LocalDate.of(2017,3,20)), is(false));
+        assertThat(userService.isAbleToGoToPub(userService.login("None","none"), LocalDate.of(2017,3,20)), is(false));
+
+    }
 
 }
